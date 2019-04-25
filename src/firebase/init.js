47684dbdebfad1d301aed445ff5c3fbd47f9cb23
Gregor Kosmina <kosmina.gregor@gmail.com>
2019-04-25 21:35:28 +0200

    improved code style

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,12 @@ import firestore from 'firebase/firestore'
 
 
 var config = {
-    apiKey: "AIzaSyC4kKKkYHSkmw0GKwiNZfeO4xt2XvSkHwo",
-    authDomain: "vue-geo-gk.firebaseapp.com",
-    databaseURL: "https://vue-geo-gk.firebaseio.com",
-    projectId: "vue-geo-gk",
-    storageBucket: "vue-geo-gk.appspot.com",
-    messagingSenderId: "295930793160"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 const firebaseApp = firebase.initializeApp(config);
 firebaseApp.firestore().settings({timestampsInSnapshots:true})
